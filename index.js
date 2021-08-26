b25c818be57024db7eb264cf70cd4605ebc8ccaf
Luca Piras <luca.piras@fielmann.com>
2021-08-26 21:41:22 +0200

    Change to class syntax

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-import assert from 'assert'
+import assert from 'assert';
 import isCidr from 'is-cidr';
 import isIp from 'is-ip';
 
-const cidrBlockCheck = {
-    v4: {
-        isInBlock: (cidrBlock, ip) => {
-            assert(isCidr.v4(cidrBlock), `${cidrBlock} is not an IPv4 CIDR block`);
-            assert(isIp.v4(ip), `${ip} is not an IPv4 address`);
+class CidrBlockCheck {
+    static get v4() {
+        return {
+            isInBlock: (cidrBlock, ip) => {
+                assert(isCidr.v4(cidrBlock), `${cidrBlock} is not an IPv4 CIDR block`);
+                assert(isIp.v4(ip), `${ip} is not an IPv4 address`);
 
-            const cidrComponents = cidrBlock.split('/');
-            const cidrPrefixLength = Number(cidrComponents[1]);
+                const cidrComponents = cidrBlock.split('/');
+                const cidrPrefixLength = Number(cidrComponents[1]);
 
-            if (cidrPrefixLength === 0) {
-                return true;
+                if (cidrPrefixLength === 0) {
+                    return true;
+                }
+
+                const cidrOctetList = cidrComponents[0].split('.').map(Number);
+                const ipOctetList = ip.split('.').map(Number);
+
+                const cidrSuffixLength = 32 - cidrPrefixLength;
+
+                let cidrUnsigned32Bit = 0;
+                let ipUnsigned32Bit = 0;
+
+                [24, 16, 8, 0].forEach((shiftBy, index) => {
+                    cidrUnsigned32Bit = cidrUnsigned32Bit + ((cidrOctetList[index] << shiftBy) >>> 0);
+                    ipUnsigned32Bit = ipUnsigned32Bit + ((ipOctetList[index] << shiftBy) >>> 0);
+                });
+
+                const cidrRemainderBits = (cidrUnsigned32Bit >>> cidrSuffixLength) >>> 0;
+                const ipRemainderBits = (ipUnsigned32Bit >>> cidrSuffixLength) >>> 0;
+
+                return cidrRemainderBits === ipRemainderBits;
             }
-
-            const cidrOctetList = cidrComponents[0].split('.').map(Number);
-            const ipOctetList = ip.split('.').map(Number);
-
-            const cidrSuffixLength = 32 - cidrPrefixLength;
-
-            let cidrUnsigned32Bit = 0;
-            let ipUnsigned32Bit = 0;
-
-            [24, 16, 8, 0].forEach((shiftBy, index) => {
-                cidrUnsigned32Bit = cidrUnsigned32Bit + ((cidrOctetList[index] << shiftBy) >>> 0);
-                ipUnsigned32Bit = ipUnsigned32Bit + ((ipOctetList[index] << shiftBy) >>> 0);
-            });
-
-            const cidrRemainderBits = (cidrUnsigned32Bit >>> cidrSuffixLength) >>> 0;
-            const ipRemainderBits = (ipUnsigned32Bit >>> cidrSuffixLength) >>> 0;
-
-            return cidrRemainderBits === ipRemainderBits;
-        }
-    }
+        };
+    };
 };
 
-export default cidrBlockCheck;
+export default CidrBlockCheck;
--- index.test.js
+++ index.test.js
@@ -1,16 +1,18 @@
-import cidrBlockCheck from './index.js';
+import CidrBlockCheck from './index.js';
 import {expect} from 'chai';
 import {describe, it} from "mocha";
-import {AssertionError} from 'assert'
+import {AssertionError} from 'assert';
 
 describe('unit tests', () => {
+    const cidrBlockCheck = new CidrBlockCheck();
+
     it('When IP addresses match Then true is returned', () => {
         // arrange
         const cidrBlock = '192.168.1.23/28';
         const ipAddress = '192.168.1.23';
 
         // act
-        const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.be.true;
@@ -22,7 +24,7 @@ describe('unit tests', () => {
         const ipAddress = '192.168.1.12';
 
         // act
-        const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.be.true;
@@ -34,7 +36,7 @@ describe('unit tests', () => {
         const ipAddress = '192.169.2.12';
 
         // act
-        const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.be.false;
@@ -46,7 +48,7 @@ describe('unit tests', () => {
         const ipAddress = '10.17.5.23';
 
         // act
-        const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.be.true;
@@ -58,7 +60,7 @@ describe('unit tests', () => {
         const ipAddress = '192.168.1.42';
 
         // act
-        const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.be.true;
@@ -70,7 +72,7 @@ describe('unit tests', () => {
         const ipAddress = '192.168.1.42';
 
         // act
-        const result = () => cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = () => CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.throw(AssertionError, /CIDR/);
@@ -82,41 +84,43 @@ describe('unit tests', () => {
         const ipAddress = 'not_a_valid_ip_address';
 
         // act
-        const result = () => cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+        const result = () => CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
         // assert
         expect(result).to.throw(AssertionError, /IP/);
     });
 
-    // describe('variant set test', () => {
-    //     [
-    //         ['55.27.53.70/0', '100.65.201.133', true],
-    //         ['128.27.53.70/1', '167.65.201.133', true],
-    //         ['128.27.53.70/1', '127.65.201.133', false],
-    //         ['56.53.60.205/2', '62.207.53.222', true],
-    //         ['191.53.60.205/2', '192.207.53.222', false],
-    //         ['100.65.201.133/3', '97.207.53.222', true]
-    //     ].forEach((testCase) => {
-    //         // arrange
-    //         const cidrBlock = testCase[0];
-    //         const ipAddress = testCase[1];
-    //         const expected = testCase[2];
+    describe('variant set test', () => {
+        [
+            ['55.27.53.70/0', '100.65.201.133', true],
+            ['128.27.53.70/1', '167.65.201.133', true],
+            ['128.27.53.70/1', '127.65.201.133', false],
+            ['56.53.60.205/2', '62.207.53.222', true],
+            ['191.53.60.205/2', '192.207.53.222', false],
+            ['100.65.201.133/3', '97.207.53.222', true]
+        ].forEach((testCase) => {
+            // arrange
+            const cidrBlock = testCase[0];
+            const ipAddress = testCase[1];
+            const expected = testCase[2];
 
-    //         it(`When CIDR is ${cidrBlock} and IP is ${ipAddress} Then ${expected} is returned`, () => {
-    //             // act
-    //             const result = cidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
+            it(`When CIDR is ${cidrBlock} and IP is ${ipAddress} Then ${expected} is returned`, () => {
+                // act
+                const result = CidrBlockCheck.v4.isInBlock(cidrBlock, ipAddress);
 
-    //             // assert
-    //             expect(result).to.equal(expected);
-    //         });
-    //     });
-    // });
+                //heapdump.writeSnapshot();
+
+                // assert
+                expect(result).to.equal(expected);
+            });
+        });
+    });
 });
 
 describe('benchmark', () => {
     const buildTestSet = (size) => {
         const set = [];
-        [...Array(size-1).keys()].forEach(() => {
+        [...Array(size-1).keys()].map(() => {
             const cidr = [0, 1, 2, 3].map(() => Math.floor(Math.random() * 255)).join('.') + '/' + (Math.floor(Math.random() * 33));
             const ip = [0, 1, 2, 3].map(() => Math.floor(Math.random() * 255)).join('.');
             set.push([cidr, ip]);
@@ -127,7 +131,7 @@ describe('benchmark', () => {
     [10000, 25000, 100000, 1000000, 10000000].forEach((size) => {
         const set =  buildTestSet(size);
         it(size.toLocaleString(), () => {
-            set.map((sample) => cidrBlockCheck.v4.isInBlock(sample[0], sample[1]));
+            set.map((sample) => CidrBlockCheck.v4.isInBlock(sample[0], sample[1]));
         }).timeout(0);
     });
 });
